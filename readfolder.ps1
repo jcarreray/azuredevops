f9968d2a0492efe662b089cc7882e24ff002738f
Juan Carlos Carrera <jccarreray@copaair.com>
2022-05-25 16:24:53 -0500

    ll

---
+++ readfolder.ps1
@@ -0,0 +1,4 @@
+$path1 = "\\idc-oslsupport-01\e$\prueba1"
+"{0:N2} MB" -f ((Get-ChildItem "$path1"| Where-Object CreationTime -LT (Get-Date).AddDays(-${days}) | Measure-Object -Property Length -Sum).Sum/ 1Mb)
+
+
