a058d9c4b7ea2c6a96d75d0039ce3d91021da6ab
Juan Carlos Carrera <jccarreray@copaair.com>
2022-05-26 08:13:30 -0500

    .

--- readfolder.ps1
+++ readfolder.ps1
@@ -1,5 +1,5 @@
 $path1 = "\\idc-oslsupport-01\e$\prueba1\ems\8.5\bin"
 $read1 = "{0:N2} MB" -f ((Get-ChildItem "$path1"| Where-Object CreationTime -LT (Get-Date).AddDays(-${days}) | Measure-Object -Property Length -Sum).Sum/ 1Mb)
-Write-Host "##vso[task.setvariable variable=read1;]$size1"
+Write-Host "##vso[task.setvariable variable=size1;]$read1"
 
 
