4dc46d897362eca822398a425ed154627b051983
Juan Carlos Carrera <jccarreray@copaair.com>
2022-05-26 16:25:48 -0500

    a

--- readfolder2.ps1
+++ readfolder2.ps1
@@ -1,4 +1,3 @@
-
-path = "\\$server\$directory"
+$path = "\\$(serverrname)\$(directory)"
 $read1 = "{0:N2} MB" -f ((Get-ChildItem "$path"| Where-Object CreationTime -LT (Get-Date).AddDays(-${days}) | Measure-Object -Property Length -Sum).Sum/ 1Mb)
 Write-Host "##vso[task.setvariable variable=size1;]$read1"
